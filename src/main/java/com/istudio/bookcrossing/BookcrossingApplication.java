29a6116e19d161b71cd8b64d1ceb3fb1535aa9b6
Kevin7985 <1298282@mail.ru>
2025-10-29 01:43:32 +0300

    Initial commit

---
+++ src/main/java/com/istudio/bookcrossing/BookcrossingApplication.java
@@ -0,0 +1,19 @@
+package com.istudio.bookcrossing;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Bookcrossing API",
+                version = "1.0.0"
+        )
+)
+public class BookcrossingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookcrossingApplication.class, args);
+    }
+}
